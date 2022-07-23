4e26790d10be869f0b959c429351b7a8cab484e7
Atul Kumbhare <atulkumbhare01@gmail.com>
2022-07-23 13:15:33 +0530

    crud operation
    todo practical

---
+++ src/pages/array.js
@@ -0,0 +1,22 @@
+const array = [
+	{
+		id:'1',
+		Name:'Shivansh',
+		Age:'23',
+		Address:'nagpur'
+	},
+	{
+		id:'2',
+		Name:'Simran',
+		Age:'22',
+		Address:'mumbai'
+	},
+	{
+		id:'3',
+		Name:'Aakash',
+		Age:'23',
+		Address:'pune'
+	}
+]
+
+export default array
